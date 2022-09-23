b7c10cc7e39de5cd393e77d804041b244b9551b0
Daniel Yu <charlesyou1234567@gmail.com>
2022-09-23 18:12:25 +0800

    典長v2.8.8

--- diangchung/js/config.js
+++ diangchung/js/config.js
@@ -427,7 +427,7 @@ safariHacks();
          bannerTitleIn()
      },4000)
   }
-
+ 
 //   var canvas = document.getElementById('canvas');
 //   var ctx = canvas.getContext('2d');
 //   var raf = requestAnimationFrame;
